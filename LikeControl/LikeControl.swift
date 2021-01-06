519a6c1714140d7db590775ad7b3c38bac30316e
Margarita Novokh <r28611@gmail.com>
2021-01-06 19:48:52 +0100

    implement LikeControl

---
+++ LikeControl/LikeControl.swift
@@ -0,0 +1,74 @@
+//
+//  LikeControl.swift
+//  gb_ui
+//
+//  Created by Margarita Novokhatskaia on 06.01.2021.
+//
+
+import UIKit
+
+@IBDesignable class LikeControl: UIControl {
+
+    private var stackView: UIStackView!
+    private let button = UIButton(type: .system)
+    var counterLabel = UILabel()
+    
+    var isLiked: Bool = false {
+        didSet {
+            self.sendActions(for: .valueChanged)
+            if oldValue == false {
+                counter += 1
+            } else {
+                counter -= 1
+            }
+            self.updateControl()
+        }
+    }
+    var counter: Int = 0
+    
+    
+    override init(frame: CGRect) {
+         super.init(frame: frame)
+        
+         self.setupView()
+     }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        self.setupView()
+    }
+    
+    private func setupView() {
+        
+        button.setImage(UIImage(systemName: "heart"), for: .normal)
+        button.setImage(UIImage(systemName: "heart.fill"), for: .selected)
+        button.addTarget(self, action: #selector(toogleIsLiked(_ :)), for: .touchUpInside)
+        
+        counterLabel.text = String(counter)
+        
+        stackView = UIStackView(arrangedSubviews: [counterLabel, button])
+
+        self.addSubview(stackView)
+
+        stackView.spacing = 3
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .equalCentering
+    }
+
+    @objc func toogleIsLiked(_ sender: UIButton) {
+        isLiked.toggle()
+    }
+    
+    private func updateControl() {
+        button.isSelected = isLiked == true
+        counterLabel.text = String(counter)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        stackView.frame = bounds
+    }
+    
+}
