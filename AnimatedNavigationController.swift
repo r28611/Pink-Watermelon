d9545ae22880ee545a0b1a0fc15d3baf5fbe8a12
Margarita Novokh <r28611@gmail.com>
2021-02-04 21:48:38 +0100

    add InteractiveTransition

--- AnimatedNavigationController.swift
+++ AnimatedNavigationController.swift
@@ -9,20 +9,42 @@ import UIKit
 
 class AnimatedNavigationController: UINavigationController, UINavigationControllerDelegate {
 
+    
+    let interactiveTransition = InteractiveTransition()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         delegate = self
+        
+        view.backgroundColor = .systemPink
 
     }
     
     func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+//        if operation == .push {
+//            return CustomPushAnimator()
+//        } else if operation == .pop {
+//            return CustomPopAnimator()
+//        }
+//        return nil
         if operation == .push {
+            self.interactiveTransition.viewController = toVC
             return CustomPushAnimator()
         } else if operation == .pop {
+            if navigationController.viewControllers.first != toVC {
+                self.interactiveTransition.viewController = toVC
+            }
             return CustomPopAnimator()
         }
         return nil
+    }
+    
+
+    func navigationController(_ navigationController: UINavigationController,
+                              interactionControllerFor animationController: UIViewControllerAnimatedTransitioning)
+                              -> UIViewControllerInteractiveTransitioning? {
+        return interactiveTransition.hasStarted ? interactiveTransition : nil
     }
 
 }
---
+++ InteractiveTransition.swift
@@ -0,0 +1,47 @@
+//
+//  InteractiveTransition.swift
+//  gb_ui
+//
+//  Created by Margarita Novokhatskaia on 04.02.2021.
+//
+
+import UIKit
+
+class InteractiveTransition: UIPercentDrivenInteractiveTransition {
+    
+    var viewController: UIViewController? {
+        didSet {
+            let recognizer = UIScreenEdgePanGestureRecognizer(target: self,
+                                                              action: #selector(handleScreenEdgeGesture(_:)))
+            recognizer.edges = [.left]
+            viewController?.view.addGestureRecognizer(recognizer)
+        }
+    }
+    
+    var hasStarted: Bool = false
+    var shouldFinish: Bool = false
+
+    @objc func handleScreenEdgeGesture(_ recognizer: UIScreenEdgePanGestureRecognizer) {
+        switch recognizer.state {
+        case .began:
+            self.hasStarted = true
+            self.viewController?.navigationController?.popViewController(animated: true)
+        case .changed:
+            let translation = recognizer.translation(in: recognizer.view)
+            let relativeTranslation = translation.x / (recognizer.view?.bounds.width ?? 1)
+            let progress = max(0, min(1, relativeTranslation))
+            
+            self.shouldFinish = progress > 0.33
+
+            self.update(progress)
+        case .ended:
+            self.hasStarted = false
+            self.shouldFinish ? self.finish() : self.cancel()
+        case .cancelled:
+            self.hasStarted = false
+            self.cancel()
+        default: return
+        }
+    }
+    
+}
