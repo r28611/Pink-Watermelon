f4b3b8d58a1ea2721a85f71031ca8bc0ab8bb52e
Margarita Novokh <r28611@gmail.com>
2021-04-01 11:54:48 +0200

    add registration form on SignupVC

--- gb_ui/SignupViewController.swift
+++ gb_ui/SignupViewController.swift
@@ -9,24 +9,44 @@ import UIKit
 
 final class SignupViewController: UIViewController {
 
-    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var goButton: UIButton!
     @IBOutlet weak var cloudView: WatermelonLoadingView!
+    @IBOutlet weak var form: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        goButton.layer.cornerRadius = goButton.frame.height / 4
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
-        cloudView.setup()
-        welcomeLabel.text = "Here will be authorization form soon! Stay tuned for more fun!"
-        welcomeLabel.numberOfLines = welcomeLabel.calculateMaxLines()
-        welcomeLabel.textColor = Constants.greenColor
+        cloudView.isHidden = true
+
+        
     }
     
-
     @IBAction func closeButton(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func goButton(_ sender: UIButton) {
+        cloudView.isHidden = false
+        animateFormDisappearing()
+        cloudView.setup()
+        Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { timer in
+            self.performSegue(withIdentifier: "to_tabBar", sender: sender)
+        }
+    }
+    
+    func animateFormDisappearing() {
+        let animation = CABasicAnimation(keyPath: "opacity")
+        animation.fromValue = 1
+        animation.toValue = 0
+        animation.duration = 0.5
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        animation.fillMode = CAMediaTimingFillMode.both
+        animation.isRemovedOnCompletion = false
+        self.form.layer.add(animation, forKey: nil)
     }
     
 }
